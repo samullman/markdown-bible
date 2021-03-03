a6dc3c13162e46f292b6f07d1dcf8d029caf58ab
Sam <samullman@gmail.com>
2021-03-03 15:16:07 -0800

    update books

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/samullman/nextra-bible',
+  titleSuffix: ' – King James Bible',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
+      <span className="mr-2 font-extrabold hidden md:inline">Nextra Bible</span>
+      {/* <span className="text-gray-600 font-normal hidden md:inline">
         The Next Site Builder
-      </span>
+      </span> */}
     </>
   ),
   head: (
@@ -15,15 +15,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
+      <meta name="description" content="Nextra — King James Bible" />
+      <meta name="og:description" content="Nextra — King James Bible" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://nextra-bible.vercel.app/bible.png" />
+      <meta name="twitter:site:domain" content="nextra-bible.vercel.app" />
+      <meta name="twitter:url" content="https://nextra-bible.vercel.app" />
+      <meta name="og:title" content="Nextra — King James Bible" />
+      <meta name="og:image" content="https://nextra.vercel.app/bible.png" />
+      <meta name="apple-mobile-web-app-title" content="Nextra Bible" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
       <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
